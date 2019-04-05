73770fbb98c4b8849addf5be2a6d98a898ac9b04
Jesus David Romero Barros <romerojesus@CAMPUSUNINORTE.LOCAL>
2019-04-05 10:37:45 -0500

    crear metodo floydwarshall

--- Floyd-Warshall/src/floyd/warshall/FloydWarshall.java
+++ Floyd-Warshall/src/floyd/warshall/FloydWarshall.java
@@ -11,9 +11,39 @@ package floyd.warshall;
  */
 public class FloydWarshall {
 
-    /**
-     * @param args the command line arguments
-     */
+    static void flowd_warshall(int grafo[][]){
+    int n=grafo.length,tmp;
+        int[][] caminos= new int [n][n];
+        //crear matriz de distnaica
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                caminos[i][j]=j;
+            }
+        }
+        //ejecutar el metodos floyd-warshall
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    tmp=grafo[i][k]+grafo[k][j];
+                    if(tmp<grafo[i][j]){
+                        grafo[i][j]=tmp;
+                        caminos[i][j]=k;
+                    }
+                }
+            }
+        }
+        System.out.println("");
+        print(caminos);
+        print(grafo);
+    }
+    static void print(int m[][]){
+        System.out.println("");
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                System.out.print(m[i][j]+" ");
+            }
+        }
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
